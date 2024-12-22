7f841e4a7d4a6e06d07e478803f7c7c58fc22108
Mohamed Talhaoui <144726758+mohamedtalhaouii@users.noreply.github.com>
2024-12-22 13:34:00 +0100

    ajouter ex2

---
+++ Factorielle.asm
@@ -0,0 +1,27 @@
+#SimuProc 1.4.2.0
+LDT "Entrer un Enrier : "
+STA 020 ; N
+STA 021 ; F
+INC 030 ; i++
+
+LDA 020
+CMP 030 ; Si ( N = i )
+JEQ 00F
+
+LDA 020
+CMP 030
+JME 00A ; Si( N < i )
+LDA 021
+MUL 030 ; F = F * i
+STA 021
+INC 030 ; i++
+JMP 004
+;Affichage
+MSG "Factorielle est : "
+LDA 021
+EAP
+HLT
+
+
+#30
+0 ; i = 0
