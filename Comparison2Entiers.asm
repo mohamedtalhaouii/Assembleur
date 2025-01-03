2d4a8fd5f3289ae2fee5d62142874ddc3867239c
Mohamed Talhaoui <144726758+mohamedtalhaouii@users.noreply.github.com>
2025-01-03 12:34:29 +0100

    ajouter exercice de comparison de deux entiers

---
+++ Comparison2Entiers.asm
@@ -0,0 +1,21 @@
+#SimuProc 1.4.2.0
+MSG "Comparison Enter deux Entiers"
+LDT "Entrer La valeur de A :"
+STA 030
+LDT "Entrer La valeur de B :"
+STA 031
+
+LDA 030
+CMP 031
+JNE 00A ;Sauter Si A != B
+MSG "A et B sont Egaux !"
+JMP 010 ;Fin
+
+LDA 030
+CMP 031
+JMA 00F ;Sauter Si A > B
+MSG "A est Plus Petite que B"
+JMP 010 ;;Fin
+
+MSG "A est Plus Grande que B"
+HLT
